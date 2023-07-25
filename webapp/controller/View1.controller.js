413a275dc56493498a96804bec388d21751c2d1e
Gabriel Lambrecht <gabriel.lambrecht@escotta.com>
2023-07-25 01:26:47 +0000

    melhorias

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,32 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
             */  
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("pricingworkflow.controller.View1", {
             onInit: function () {
 
-            }
+            },
+
+            onChangeFilters: function(oEvent){
+                debugger
+                let selectedStatus = oEvent.getSource().getSelectedKey()
+                
+                var oFilter = new Filter(
+                    "Status",
+                    FilterOperator.Contains,
+                    selectedStatus
+                );
+
+                this.byId("ApprovalTable").getBinding("items").filter([oFilter])
+            
+            }   
+
         });
     });
